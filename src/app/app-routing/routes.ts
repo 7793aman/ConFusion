9facecd4878758484143048376def9812942f3e5
Aman Jain <7793aman@gmail.com>
2019-12-31 11:05:04 +0530

    spa part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component'
 import { MenuComponent } from '../menu/menu.component';
-
+import { DishdetailComponent } from '../dishdetail/dishdetail.component'
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'menu', component: MenuComponent },
-    { path: '', redirectTo: '/home' ,pathMatch:'full'}
+    { path: 'dishdetail/:id', component: DishdetailComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 
 ]
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit ,Input } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import {Dish} from '../shared/dish'
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {DishService} from '../services/dish.service';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -7,13 +11,19 @@ import {Dish} from '../shared/dish'
 })
 export class DishdetailComponent implements OnInit {
   
-  @Input()
-  oneWayBindedDish:Dish;
-  
-  constructor() { }
+  //@Input()
+  //oneWayBindedDish:Dish;
+  dish:Dish;
+  constructor(private dishService:DishService,
+    private location:Location,private route:ActivatedRoute ) {}
 
   ngOnInit() {
-  
+    let id=this.route.snapshot.params['id'];
+    this.dish=this.dishService.getDish(id);
+  }
+
+  goBack():void{
+    this.location.back();
   }
 
 }
